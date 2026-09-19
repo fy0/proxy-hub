a6a195f661f581cc36903cb216b70e7f00b4eadc
fy <fy0748@gmail.com>
2026-09-20 01:07:24 +0800

    chore(release): prepare version 1.1.0

--- version.go
+++ version.go
@@ -7,7 +7,7 @@ var (
 	VERSION = semver.MustParse(VERSION_MAIN + VERSION_PRERELEASE + VERSION_BUILD_METADATA)
 
 	// VERSION_MAIN is the main semantic version.
-	VERSION_MAIN = "1.0.1"
+	VERSION_MAIN = "1.1.0"
 	// VERSION_PRERELEASE is the semantic version prerelease suffix.
 	VERSION_PRERELEASE = ""
 	// VERSION_BUILD_METADATA is the semantic version build metadata suffix.
